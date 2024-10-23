28ad4c4f5da9eaed9fc9bb9cdf0a4399a5a71331
Mal Minhas <mal@malm.co.uk>
2024-10-23 01:29:24 +0100

    Updated test code

--- test_rusty.py
+++ test_rusty.py
@@ -1,25 +1,30 @@
 import time
 import rusty
 
-def factorial(n):
-    if n <= 1:
-        return n
-    return n * factorial(n - 1)
+K=2000000
 
 def test_nonrusty(n):
-    [factorial(n) for i in range(100000)]
+    """ test function for Python implementation """
+    def factorial(n):
+        if n <= 1:
+            return n
+        return n * factorial(n - 1)
+    [factorial(n) for i in range(K)]
     
 def test_rusty(n):
-    """ test function """
-    [rusty.factorial(n) for i in range(100000)]
+    """ test function for Rust implementation """
+    [rusty.factorial(n) for i in range(K)]
     
 if __name__ == '__main__':
-    n = 35
+    n = 34
     start = time.time()
     test_rusty(n)
     end = time.time()
-    print(f"time for rust code: {end-start}")
+    rust_time = end-start
+    print(f"time for rust code: {round(rust_time,2)} sec")
     start = time.time()
     test_nonrusty(n)
     end = time.time()
-    print(f"time for python code: {end-start}")
+    python_time = end-start
+    print(f"time for python code: {round(python_time,2)} sec")
+    print(f"Rust implementation is {round(python_time/rust_time,2)} times faster than Python one")
